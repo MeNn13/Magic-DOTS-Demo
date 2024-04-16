17bb14a5296dcbb2e16dddd3fc97f210f36e9aec
MeNn13 <banzai.davai@gmail.com>
2024-04-16 15:28:39 +0300

    Initial commit

---
+++ Assets/Code/ECS/Authoring/CubeSpawnerAuthoring.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Unity.Entities;
+
+namespace Assets.Code.ECS.Authoring
+{
+    public class CubeSpawnerAuthoring : MonoBehaviour
+    {
+        [SerializeField] private GameObject _prefab;
+        [SerializeField] private float _spawnRate;
+
+
+    }
+
+
+}
---
+++ Assets/Code/ECS/Components/CubeSpawnerComponent.cs
@@ -0,0 +1,13 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+namespace Assets.Code.ECS.Components
+{
+    public struct CubeSpawnerComponent : IComponentData
+    {
+        public Entity Prefab;
+        public float3 SpawnPosition;
+        public float NextSpawnRate;
+        public float SpawnRate;
+    }
+}
